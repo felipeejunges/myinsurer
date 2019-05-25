ca03f034b573496cab6259fd7aebc91cb6ba1f27
PUCPR <pucpr@LADMAC0122.local>
2019-05-25 09:59:08 -0300

    Alteracoes pt1

---
+++ MyInsurer/Model/Benefit.swift
@@ -0,0 +1,17 @@
+//
+//  Benefit.swift
+//  MyInsurer
+//
+//  Created by PUCPR on 25/05/19.
+//  Copyright © 2019 PUCPR. All rights reserved.
+//
+
+import Foundation
+
+class Benefit {
+    var nome = ""
+    var provedor = ""
+    var dataInicio = ""
+    var dataFim = ""
+    var dependentes = [Dependente]()
+}
---
+++ MyInsurer/Model/Dependente.swift
@@ -0,0 +1,14 @@
+//
+//  Dependente.swift
+//  MyInsurer
+//
+//  Created by PUCPR on 25/05/19.
+//  Copyright © 2019 PUCPR. All rights reserved.
+//
+
+import Foundation
+
+class Dependente {
+    var nome = ""
+    var nascimento = ""
+}
